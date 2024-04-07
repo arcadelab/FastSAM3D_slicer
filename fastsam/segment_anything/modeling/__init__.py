0ad6ec785892ac5ffffc37dc4689234c8d2fdbab
swedfr <95553597+swedfr@users.noreply.github.com>
2024-04-07 18:35:16 -0400

    Delete fastsam directory

--- fastsam/segment_anything/modeling/__init__.py
+++
@@ -1,6 +0,0 @@
-from .sam3D import Sam3D
-from .image_encoder3D import ImageEncoderViT3D
-from .mask_decoder3D import MaskDecoder3D, TwoWayTransformer3D
-from .prompt_encoder3D import PromptEncoder3D
-
-from .transformer import TwoWayTransformer
